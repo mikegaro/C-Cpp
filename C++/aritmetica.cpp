67928d2b4d1df031d1752c9403149f75f4d04cdb
Miguel <garcia.migue.onate@gmail.com>
2020-04-29 00:14:59 -0500

    nuevo

---
+++ C++/aritmetica.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  double valor1 = (double)7/2;
+  cout << valor1 << endl;
+
+  int valor2 = (int)7.3;
+  cout << valor2 << endl;
+
+  int valor3 = 8;
+  cout << valor3 << endl;
+
+  int valor4 = 10;
+  valor4 /= 5;
+  cout << valor4 << endl;
+
+  int valor5 = 13%5;
+  cout << valor5 << endl;
+
+  double ecuacion = 5.3/4 + 2*6;
+  cout << ecuacion << endl;
+
+
+  return 0;
+}

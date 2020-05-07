a621aee9f44ca0d154ca409e8ceac0fbfe408c31
mikegaro <garcia.miguel.onate@gmail.com>
2020-05-06 19:22:47 -0500

    ya arregle mi compilador jejej

---
+++ C++/herencia.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+class Animal{
+public:
+  void hablar(){cout << "GRRRR" << endl;}
+};
+
+//Esta es una subclase de Animal
+class Gato: public Animal {
+  //La clase gato hereda todos los metodos publicos de Animal
+  //ademas de que Gato puede tener sus propios metodos
+public:
+  void saltar(){cout << "Miau estoy saltando" << endl;}
+};
+
+//Esta es una subclase de Gato que a su vez es una subclase de Animal
+class Tigre: public Gato{
+//La clase Tigre hereda todos los metodos publicos de Gato
+//ademas de que Tigre puede tener sus propios metodos.
+public:
+  void fuerteRugido(){cout << "ROAAAAAAR" << endl;}
+};
+
+int main(){
+
+  Animal a;
+  a.hablar();
+
+  Gato b;
+  b.hablar();
+  b.saltar();
+
+  Tigre c;
+  c.hablar();
+  c.saltar();
+  c.fuerteRugido();
+
+  return 0;
+}

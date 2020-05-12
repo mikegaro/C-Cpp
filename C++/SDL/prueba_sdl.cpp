aefee2051fe79d04e5ddf0ec5bea3f59a7b2e93c
mikegaro <garcia.miguel.onate@gmail.com>
2020-05-11 20:38:31 -0500

    jeje

--- C++/SDL/prueba_sdl.cpp
+++ C++/SDL/prueba_sdl.cpp
@@ -3,6 +3,20 @@
 #include<SDL2/SDL.h>
 #include<iostream>
 
+/*
+Para poder compilar, se necesita customizar el compilador con el siguiente
+comando:
+
+g++ archivo.cpp -IC:\SDL\include -LC:\SDL\lib -lmingw32 -lSDL2main -lSDL2 -o objeto
+
+Es decir, se necesita especificar en donde se encuentran los headers(include) y
+las librerias. Ademas, se debe añadir el archivo .dll a la carpeta \bin\ del
+compilador
+
+ya se ya se, cuanta pinche cosa para que una tonta ventana aparezca.
+
+*/
+
 using namespace std;
 
 int main(){

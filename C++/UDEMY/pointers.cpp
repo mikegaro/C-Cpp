9876517e0929d192db9ef9a62928c40d6e6e9402
Mike <garcia.miguel.onate@gmail.com>
2021-08-08 21:04:34 -0500

    hola

--- C++/UDEMY/pointers.cpp
+++ C++/UDEMY/pointers.cpp
@@ -3,6 +3,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int score{100};
+    int *score_ptr{&score};
+    cout << *score_ptr << endl;
+    *score_ptr = 200;
+
+    //----------------------------
+    //DINAMIC MEMORY ALLOCATION
+    int *array_ptr{nullptr};
+    array_ptr = new int[10];
+
+    delete[] array_ptr;
+
+    cout << *score_ptr << endl;
+    cout << score << endl;
+
     return 0;
 }

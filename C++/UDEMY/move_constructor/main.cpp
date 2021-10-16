5b601c1f3419f5fe8fe829141b3ef9dd955d2214
Mike <garcia.miguel.onate@gmail.com>
2021-10-15 21:34:00 -0500

    hola

--- C++/UDEMY/move_constructor/main.cpp
+++ C++/UDEMY/move_constructor/main.cpp
@@ -11,6 +11,7 @@ class MoveCopy {
     int get_data_value( ) { return *data; };
     MoveCopy( int d );
     MoveCopy( const MoveCopy & source );
+    MoveCopy( MoveCopy && source ) noexcept;
     ~MoveCopy( );
 };
 
@@ -25,8 +26,34 @@ MoveCopy::~MoveCopy( ) {
     cout << "Destructor..." << endl;
 }
 
+MoveCopy::MoveCopy( MoveCopy && source ) noexcept : data{ source.data } {
+    source.data = nullptr; //"Robamos" el valor de data y borramos lo que haya en source
+    cout << "Move constructor -> Moviendo recurso" << endl;
+}
+
+
+MoveCopy::MoveCopy( const MoveCopy & source ) : MoveCopy{ *source.data } {
+    cout << "Copy Constructor" << endl;
+}
+
 int main( ) {
-    Vector<MoveCopy> vec;
+    vector<MoveCopy> vec;
+    cout << "." << endl;
+    vec.push_back( MoveCopy{ 10 } );
+    /* Dado que estamos pasando un R-value(MoveCopy{10}) en lugar de un L-value(variable)
+       se va a generar un objeto temporal(MoveCopy{10}) y cuando push_back use este objeto
+       el copy constructor va a generar otro objeto igual, lo cual es ineficiente
+     */
+    cout << "." << endl;
+    vec.push_back( MoveCopy{ 20 } );
+    /*Ahora, push_back por cada nuevo elemento, va recorriendo el siguiente por lo que cada
+      vez que agregamos un segundo elemento, se generan dos copias nuevas de esos objetos */
+    cout << "." << endl;
+    /*Ahora se generan tres copias con este nuevo elemento que se recorren*/
+    vec.push_back( MoveCopy{ 30 } );
+    cout << "." << endl;
+    //Con el move constructor el recurso se "mueve" a una nueva direccion en lugar de hacer copias
+
     return 0;
 }
 

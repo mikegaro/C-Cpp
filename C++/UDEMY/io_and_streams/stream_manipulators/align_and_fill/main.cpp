bfe553b57c30d1f98fa08c63bef2e49c5bc71da5
Mike <garcia.miguel.onate@gmail.com>
2022-03-20 14:53:08 -0600

    rare changes to sth

--- C++/UDEMY/io_and_streams/stream_manipulators/align_and_fill/main.cpp
+++ C++/UDEMY/io_and_streams/stream_manipulators/align_and_fill/main.cpp
@@ -18,7 +18,7 @@ int main( ) {
     std::cout << "\n-------- DEFAULTS -------------" << std::endl;
     std::cout << num1 << num2 << hello << std::endl;
 
-    std::cout << "\n-------- DEFAULTS -------------" << std::endl;
+    std::cout << "\n-------- SET WIDTH -------------" << std::endl;
     std::cout << std::setw( 10 ) << num1;
     std::cout << std::setw( 10 ) << num2 << hello << std::endl;
 
